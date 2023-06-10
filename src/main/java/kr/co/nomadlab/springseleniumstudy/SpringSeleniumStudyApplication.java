eb485d3a772f38deece995d680f8532253711b37
nomadlab-busan <nomadlab.busan@gmail.com>
2023-06-10 20:25:38 +0900

    Initial commit

---
+++ src/main/java/kr/co/nomadlab/springseleniumstudy/SpringSeleniumStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.nomadlab.springseleniumstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeleniumStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSeleniumStudyApplication.class, args);
+    }
+
+}
